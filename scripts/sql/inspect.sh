a9171320113dbd9d40a12ef6b5539ee8a80edd2c
ivankp <ivan.pogrebnyak@gmail.com>
2015-12-10 11:55:28 -0500

    add scripts/sql/add_bh_A2j

--- scripts/sql/inspect.sh
+++ scripts/sql/inspect.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "num rows "
-sqlite3 ntuples.db "SELECT Count(*) FROM bh"
+sqlite3 ~/disk2/ntuples.db "SELECT Count(*) FROM bh"
 
 for col in particle dset process energy part jet_pt_cut jet_eta_cut other_cuts
 do
